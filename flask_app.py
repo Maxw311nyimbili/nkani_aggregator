39bfc39602df0339c27f71cbf5ac4d4eafa2ef39
Maxw311 <nyimbilimaxwell9@gmail.com>
2024-12-15 16:46:53 +0000

    add minor adjustment

--- flask_app.py
+++ flask_app.py
@@ -260,17 +260,11 @@ def delete_comment():
 
 
 
-@app.route('/get_comments', methods=['GET', 'POST'])
+@app.route('/get_comments', methods=['POST'])
 def get_comments():
 
-    article_link = None  # Initialize variable to avoid NameError
-
-    if request.method == 'POST':
-        data = request.get_json()
-        article_link = data.get('article_id')
-
-    elif request.method == 'GET':
-        article_link = request.args.get('article_id')  # Get article ID from query parameter
+    data = request.get_json()
+    article_link = data.get('article_id')
 
     if not article_link:
         return jsonify({'success': False, 'message': 'Article ID is required.'}), 400
